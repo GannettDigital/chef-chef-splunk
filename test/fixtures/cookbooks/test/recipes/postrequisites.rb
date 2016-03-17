28129961db5991c26e7022879084876b1449664f
Ron Lipke <rlipke@gannett.com>
2016-03-17 13:17:37 -0400

    GDAES-3436 Add service timeout because windows

--- test/fixtures/cookbooks/test/recipes/postrequisites.rb
+++ test/fixtures/cookbooks/test/recipes/postrequisites.rb
@@ -1,24 +1,3 @@
-splunk_app 'test-upgrade' do
-  app_name 'test'
-  if node['platform_family'] == 'windows'
-    splunk_auth 'admin:changeme'
-  else
-    splunk_auth 'admin:notarealpassword'
-  end
-  cookbook_file 'test-0.0.2.spl'
-  action [:install, :enable]
-end
-
-splunk_app 'sanitycheck2-delete' do
-  app_name 'sanitycheck2'
-  if node['platform_family'] == 'windows'
-    splunk_auth 'admin:changeme'
-  else
-    splunk_auth 'admin:notarealpassword'
-  end
-  action :remove
-end
-
 splunk_app 'bistro-upgrade' do
   app_name 'bistro'
   if node['platform_family'] == 'windows'
@@ -32,6 +11,27 @@ splunk_app 'bistro-upgrade' do
 end
 splunk_app 'bistro-remote-file-delete' do
   app_name 'bistro-1.0.2'
+  if node['platform_family'] == 'windows'
+    splunk_auth 'admin:changeme'
+  else
+    splunk_auth 'admin:notarealpassword'
+  end
+  action :remove
+end
+
+splunk_app 'test-upgrade' do
+  app_name 'test'
+  if node['platform_family'] == 'windows'
+    splunk_auth 'admin:changeme'
+  else
+    splunk_auth 'admin:notarealpassword'
+  end
+  cookbook_file 'test-0.0.2.spl'
+  action [:install, :enable]
+end
+
+splunk_app 'sanitycheck2-delete' do
+  app_name 'sanitycheck2'
   if node['platform_family'] == 'windows'
     splunk_auth 'admin:changeme'
   else
